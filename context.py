20d495dc0417ebf38918b3edcd1602f7ccc070a6
oscar88roman <161612206+oscar88roman@users.noreply.github.com>
2024-09-08 17:00:33 +0200

    Add files via upload

---
+++ context.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+
+#add parent directory
+sys.path.append('../')
+sys.path.append ('/Users/oscarroman/Library/CloudStorage/OneDrive-FondazioneBrunoKessler')
+sys.path.append ('/Users/oscarroman/Library/CloudStorage/OneDrive-FondazioneBrunoKessler/Building 3D_Challenge')
+sys.path.append ('/Users/oscarroman/Library/CloudStorage/OneDrive-FondazioneBrunoKessler/Building 3D_Challenge/utils')
+sys.path.append ('/Users/oscarroman/Library/CloudStorage/OneDrive-FondazioneBrunoKessler/Building 3D_Challenge/utils/utils_ChallengeBuilding3D.py')
+sys.path.append('/Users/oscarroman/Library/CloudStorage/OneDrive-FondazioneBrunoKessler/Building 3D_Challenge/data')
+sys.path.append('/Users/oscarroman/Library/CloudStorage/OneDrive-FondazioneBrunoKessler/Building 3D_Challenge/script')
+
